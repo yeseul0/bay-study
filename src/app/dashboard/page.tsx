e9e92d55d285bb1a68a824a737851afd175c13d5
yeseul0 <choiyeseul@g.hongik.ac.kr>
2025-10-25 20:24:12 +0900

    build error

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -68,7 +68,7 @@ export default function DashboardPage() {
       window.ethereum.on('chainChanged', handleChainChanged);
 
       return () => {
-        window.ethereum.removeListener('chainChanged', handleChainChanged);
+        window.ethereum?.removeListener('chainChanged', handleChainChanged);
       };
     }
   }, []);
